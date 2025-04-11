13af9e94570a186ef7facfebc8f6672fda4636dd
Leo <leonsiypopov@gmail.com>
2025-04-11 09:36:54 +0300

    problem

--- cinema/views.py
+++ cinema/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.models import AbstractUser, AnonymousUser
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
-from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession
+from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order, Ticket
 
 from cinema.serializers import (
     GenreSerializer,
@@ -11,7 +13,7 @@ from cinema.serializers import (
     MovieSessionListSerializer,
     MovieDetailSerializer,
     MovieSessionDetailSerializer,
-    MovieListSerializer,
+    MovieListSerializer, OrderSerializer, TicketSerializer,
 )
 
 
@@ -56,3 +58,38 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
             return MovieSessionDetailSerializer
 
         return MovieSessionSerializer
+
+
+class TicketsViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+
+class OrderListPagination(PageNumberPagination):
+    page_size = 2
+    max_page_size = 2
+    page_size_query_param = "page"
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    pagination_class = OrderListPagination
+
+    def get_serializer_class(self):
+        return OrderSerializer
+
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        return queryset
+
+        # if isinstance(self.request.user, AnonymousUser):
+        #     return queryset
+        # return queryset.filter(user=self.request.user)
+
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
